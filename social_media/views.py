7d7bb726ad022e935eeb5586df551ef6707465c2
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2025-01-06 01:52:10 +0200

    add social_media/views.py & user/views.py

--- social_media/views.py
+++ social_media/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from social_media.models import Profile
+from social_media.serializers import ProfileSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.action == "list":
+            username = self.request.query_params.get("username")
+            if username:
+                queryset = queryset.filter(username__icontains=username)
+            return queryset.distinct()
+        else:
+            return self.queryset.filter(user=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "username",
+                type=OpenApiTypes.STR,
+                description="Filter by username (ex. ?username=name)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to profile"""
+        profile = self.get_object()
+        serializer = self.get_serializer(profile, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
